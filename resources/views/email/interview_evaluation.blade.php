4838c3507a3b4f26c77b205a11a80680412946c4
sanjaykumarLaravel <96184409+sanjaykumarLaravel@users.noreply.github.com>
2022-07-22 11:58:49 +0530

    email template format change (#3)
    Co-authored-by: sanjay <sanjay.kumar@reseller.netprophetsglobal.com>

--- resources/views/email/interview_evaluation.blade.php
+++ resources/views/email/interview_evaluation.blade.php
@@ -4,13 +4,14 @@
     <title>NetProphets Cyberworks Pvt. Ltd.</title>
 </head>
 <body>
-    <p>Hi,</p>
+    <p>Hi {{$name}},</p>
     </br>
     </br>
         <p>Please give your feedback regarding {{$candidate_name}}.</p>
     </br>	
     <p>
-    	<a href="http://localhost:8000/interview-evaluation-feedback/{{$interview_evaluation_id}}/{{$interviewerString}}">http://localhost:8000/interview-evaluation-feedback/{{$interview_evaluation_id}}/{{$interviewerString}}</a>
+        <b>Note:<b> Please take this up urgently.</br><br>
+    	<a href="http://localhost:8000/interview-evaluation-feedback/{{$interview_evaluation_id}}/{{$interviewerString}}">Click Here</a>
     </p>
     </br>
     </br>
