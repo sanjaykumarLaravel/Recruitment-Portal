5d32935b627b360a89f8262a7fc3200383dd11c2
sanjaykumarLaravel <96184409+sanjaykumarLaravel@users.noreply.github.com>
2022-08-22 12:20:03 +0530

    Ankit (#5) (#6)
    * feedback
    
    * changes in interview evaluation
    
    * email improvment
    
    * code improvement
    
    * code improvement
    
    * email template format change (#3)
    
    Co-authored-by: sanjay <sanjay.kumar@reseller.netprophetsglobal.com>
    
    * bug solved
    
    * Ankit (#4)
    
    * email improvment
    
    * code improvement
    
    * code improvement
    
    * bug solved
    
    Co-authored-by: ramesh@papa@4433 <ankit.katiyar@netprophetsglobal.com>
    
    * all file commit csv
    
    Co-authored-by: ramesh@papa@4433 <ankit.katiyar@netprophetsglobal.com>
    Co-authored-by: ankitkatiyarnetprophetsglobal <96762410+ankitkatiyarnetprophetsglobal@users.noreply.github.com>
    Co-authored-by: sanjay <sanjay.kumar@reseller.netprophetsglobal.com>
    
    Co-authored-by: ramesh@papa@4433 <ankit.katiyar@netprophetsglobal.com>
    Co-authored-by: ankitkatiyarnetprophetsglobal <96762410+ankitkatiyarnetprophetsglobal@users.noreply.github.com>
    Co-authored-by: sanjay <sanjay.kumar@reseller.netprophetsglobal.com>

--- resources/views/include/sidebar.blade.php
+++ resources/views/include/sidebar.blade.php
@@ -53,6 +53,14 @@
             <span class="nav-link-text ms-1">Interview Evaluation</span>
           </a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link text-white {{  (Request::segment(1) ==='employee-list' || Request::segment(1) ==='employee-list' || Request::segment(1) ==='employee-list') ? 'active bg-gradient-primary' : '' }}" href="{{route('employee-list')}}">
+            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="material-icons opacity-10">receipt_long</i>
+            </div>
+            <span class="nav-link-text ms-1">Employee List</span>
+          </a>
+        </li>
 
         @endif
 
